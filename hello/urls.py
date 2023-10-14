b241950ce30034f64508c656e9968afefcecf418
rJinno <ma112358th13@gmail.com>
2023-10-14 19:06:00 +0900

    むずい

--- hello/urls.py
+++ hello/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index3, name='index3'),
+    path('', views.index4, name='index4'),
+    path('form', views.form, name='form'),
     path('<int:id>/<nickname>/', views.index2, name = 'index2'),
     path('next', views.next, name='next'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
+from .forms import HelloForm
 # Create your views here.
 
 def index1(request):
@@ -31,3 +33,32 @@ def next(request):
         'goto':'index3',
     }
     return render(request, 'hello/index.html', params)
+
+def index4(request):
+    params={
+        'title':'Hello/Index',
+        'msg':'お名前は？',
+    }
+    return render(request, 'hello/index.html', params)
+
+def form(request):
+    msg=request.POST['msg']
+    params={
+        'title':'Hello/Form',
+        'msg':'こんにちは'+msg,
+    }
+    return render(request, 'hello/index.html', params)
+
+def index5(request):
+    params = {
+        'title': 'Hello',
+        'message': 'your data:',
+        'form': HelloForm()
+    }
+    if(request.method == 'POST'):
+        params['message'] = '名前：' + request.POST['name']+\
+            '<br>メール:'+ request.POST['mail']+\
+            '<br>年齢：'+request.POST['age']
+        params['form'] = HelloForm(request.POST)
+    return render(request, 'hello/index.html', params)
+
